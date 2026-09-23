ad3df449e533370aeb49967feffe92f5f6aac858
George Geng <george@Mac.lan>
2026-09-23 12:31:46 -0700

    initial commit week 2 hw

---
+++ sketch.js
@@ -0,0 +1,21 @@
+let x, y, d;
+
+function setup() {
+  // Create a canvas that fills the entire browser window
+  createCanvas(windowWidth, windowHeight);
+
+  x = 0;
+  y = height / 2;
+  d = 50;
+}
+
+function draw() {
+  // Set background
+  background(0);
+
+  fill(255 * (1 - x / width), 255 * (x / width), 0); // from red to green
+  circle(x, y, (d / width) * x); // from 0 to d
+
+  x += 3;
+  x %= width; // loop around
+}
