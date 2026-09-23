aa176130294de6738b36ee443c94e5fa133aaed0
George Geng <george@Mac.lan>
2026-09-23 12:45:06 -0700

    heigh dynamic

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let x, y, d;
+let x, y;
 
 function setup() {
   // Create a canvas that fills the entire browser window
@@ -6,12 +6,14 @@ function setup() {
 
   x = 0;
   y = height / 2;
-  d = height / 5;
 }
 
 function draw() {
   // Set background
   background(0);
+
+  // dynamic diameter (10% of current canvas height)
+  let currentD = height / 10;
 
   fill(255 * (1 - x / width), 255 * (x / width), 0); // from red to green
   circle(x, y, (d / width) * x); // from 0 to d
